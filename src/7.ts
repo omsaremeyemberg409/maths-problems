cee552945d61be16f8373d4135d6f1afc234074e
omsaremeyemberg409 <omsaremeyemberg409@gmail.com>
2025-03-13 10:29:23 +0000

    Add 7.ts

---
+++ src/7.ts
@@ -0,0 +1,51 @@
+<details>
+<summary>Problem 1</summary>
+<p>
+Find the value of x in the equation:<br>
+3x + 5 = 20<br>
+
+Solution:<br>
+Subtract 5 from both sides: <br>
+3x = 15<br>
+Divide by 3 on both sides:<br>
+x = 5<br>
+Therefore, the value of x is 5.
+</p>
+</details>
+
+<details>
+<summary>Problem 2</summary>
+<p>
+Find the area of a rectangle if its length is 5 cm and width is 3 cm.
+
+Solution:<br>
+The area of a rectangle can be found by multiplying the length and width: <br>
+Area = Length x Width = 5 x 3 = 15 square centimeters.
+</p>
+</details>
+
+<details>
+<summary>Problem 3</summary>
+<p>
+Find the value of y in the equation:<br>
+y = 2x + 1<br>
+
+Solution:<br>
+Substitute x = 4 into the equation: <br>
+y = 2(4) + 1 = 9.
+</p>
+</details>
+
+<details>
+<summary>Problem 4</summary>
+<p>
+Find the value of a in the equation:<br>
+a/2 + b/3 = 5<br>
+
+Solution:<br>
+Multiply both sides by 6 to isolate a: <br>
+a + b = 30<br>
+Since we know that a = 15 and b = 10, we can find the value of a:<br>
+a = 15.
+</p>
+</details>
