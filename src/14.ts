12f212abf1b79f87aef6f50df571638b339dd154
omsaremeyemberg409 <omsaremeyemberg409@gmail.com>
2025-03-20 05:45:24 +0000

    Add 14.ts

---
+++ src/14.ts
@@ -0,0 +1,75 @@
+// Math Problems Collection
+const mathProblems = {
+  add(a: number, b: number): number {
+    return a + b;
+  },
+  subtract(a: number, b: number): number {
+    return a - b;
+  },
+  multiply(a: number, b: number): number {
+    return a * b;
+  },
+  divide(a: number, b: number): number {
+    return a / b;
+  }
+};
+
+const mathProblems1 = {
+  add(a: number, b: number): number {
+    return a + b;
+  },
+  subtract(a: number, b: number): number {
+    return a - b;
+  },
+  multiply(a: number, b: number): number {
+    return a * b;
+  },
+  divide(a: number, b: number): number {
+    return a / b;
+  }
+};
+
+const mathProblems2 = {
+  add(a: number, b: number): number {
+    return a + b;
+  },
+  subtract(a: number, b: number): number {
+    return a - b;
+  },
+  multiply(a: number, b: number): number {
+    return a * b;
+  },
+  divide(a: number, b: number): number {
+    return a / b;
+  }
+};
+
+const mathProblems3 = {
+  add(a: number, b: number): number {
+    return a + b;
+  },
+  subtract(a: number, b: number): number {
+    return a - b;
+  },
+  multiply(a: number, b: number): number {
+    return a * b;
+  },
+  divide(a: number, b: number): number {
+    return a / b;
+  }
+};
+
+const mathProblems4 = {
+  add(a: number, b: number): number {
+    return a + b;
+  },
+  subtract(a: number, b: number): number {
+    return a - b;
+  },
+  multiply(a: number, b: number): number {
+    return a * b;
+  },
+  divide(a: number, b: number): number {
+    return a / b;
+  }
+};
