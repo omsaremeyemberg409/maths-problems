ea2ae8664602120160f97dd59174e58184108400
omsaremeyemberg409 <omsaremeyemberg409@gmail.com>
2025-05-17 07:39:07 +0000

    Add 34.ts

---
+++ src/34.ts
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Example 1: Simple multiplication
+result = 3 * 4
+print(result)
+
+# Example 2: Addition of two numbers
+number1 = 5
+number2 = 7
+sum_result = number1 + number2
+print(sum_result)
+
+# Example 3: Subtraction of two numbers
+difference_result = 9 - 6
+print(difference_result)
